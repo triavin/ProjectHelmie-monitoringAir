8da9458b1b0c2e0c935121ea5e21bd1f22f15467
Aldi Triavin <aldi3avin@gmail.com>
2023-03-27 22:30:23 +0700

    add test-relay file

---
+++ ino file/test/test_relay/README_First.txt
@@ -0,0 +1,12 @@
+Relay 6 channel pinOut
+
+1.  in 1 => D4
+2.  in 2 => D5
+3.  in 3 => D6
+4.  in 4 => D7
+5.  in 5 => D8
+6.  in 6 => D9
+
+Relay 1 channel pinOut
+
+1. in => D10
---
+++ ino file/test/test_relay/test_relay.ino
@@ -0,0 +1,65 @@
+// inisialisasi variabel relay
+
+const int Relay1 = 4;
+const int Relay2 = 5;
+const int Relay3 = 6;
+const int Relay4 = 7;
+const int Relay5 = 8;
+const int Relay6 = 9;
+const int Relay7 = 10;
+
+void setup() {
+  // set pin mode to output
+
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(Relay3, OUTPUT);
+  pinMode(Relay4, OUTPUT);
+  pinMode(Relay5, OUTPUT);
+  pinMode(Relay6, OUTPUT);
+  pinMode(Relay7, OUTPUT);
+
+  relayOff();
+}
+
+void relayOnAndOff(){
+  digitalWrite(Relay1, LOW);
+  delay(3000);
+  digitalWrite(Relay1, HIGH);
+  digitalWrite(Relay2, LOW);
+  delay(3000);
+  digitalWrite(Relay2, HIGH);
+  digitalWrite(Relay3, LOW);
+  delay(3000);
+  digitalWrite(Relay3, HIGH);
+  digitalWrite(Relay4, LOW);
+  delay(3000);
+  digitalWrite(Relay4, HIGH);
+  digitalWrite(Relay5, LOW);
+  delay(3000);
+  digitalWrite(Relay5, HIGH);
+  digitalWrite(Relay6, LOW);
+  delay(3000);
+  digitalWrite(Relay6, HIGH);
+  digitalWrite(Relay7, LOW);
+  delay(3000);
+  digitalWrite(Relay7, HIGH);
+}
+
+
+void relayOff(){
+  digitalWrite(Relay1, HIGH);
+  digitalWrite(Relay2, HIGH);
+  digitalWrite(Relay3, HIGH);
+  digitalWrite(Relay4, HIGH);
+  digitalWrite(Relay5, HIGH);
+  digitalWrite(Relay6, HIGH);
+  digitalWrite(Relay7, HIGH);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  relayOnAndOff();
+  delay(1000);
+
+}
