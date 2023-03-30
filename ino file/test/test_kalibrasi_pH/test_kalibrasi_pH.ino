67092cc4e796a0c6823405a7c51dd4f435398a64
Aldi Triavin <aldi3avin@gmail.com>
2023-03-30 20:23:38 +0700

    update beberapa nilai dan note

--- ino file/test/test_kalibrasi_pH/test_kalibrasi_pH.ino
+++ ino file/test/test_kalibrasi_pH/test_kalibrasi_pH.ino
@@ -13,18 +13,19 @@ void setup() {
 void loop() {
   // membaca Nilai Sensor pH
   Nilai_Sensor_pH = analogRead(sensorPin);
-  Tegangan_Sensor_pH = 5 / 1024.0 * Nilai_Sensor_pH;
+  Tegangan_Sensor_pH = Nilai_Sensor_pH * (5 / 1023.0) ;
   Serial.print("Nilai Sensor pH : ");
   Serial.print(Nilai_Sensor_pH);
   Serial.print(" | Nilai Tegangan Sensor pH : ");
   Serial.println(Tegangan_Sensor_pH);
   
   // delay 1 menit
-  delay(60000);
+  delay(1000);
 }
 
-// note
-// nilai tegangan untuk pH 7  : 2.53 +- 0.01
+// note 
+// jika menggunakan tegangan 5v
+// nilai tegangan untuk pH 7  : 2.63 +- 0.01
 // nilai untuk pH 7           : 519 +- 1
-// nilai tegangan untuk pH 4  : 3.03 +- 0.04
+// nilai tegangan untuk pH 4  : 3.036 +- 0.04
 // nilai untuk pH 4           : 621 +- 8
